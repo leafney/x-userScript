4a1ab78d2607e3fb33b5adf97a7b6a2f7416c5c4
leafney <babycoolzx@126.com>
2022-05-27 17:56:52 +0800

    jdcookie test

--- JDCookie.user.js
+++ JDCookie.user.js
@@ -38,14 +38,12 @@
 
     function getCookies2(cookieStr, names) {
         let res = '';
-        // var cookieStr = document.cookie;
         var cas = cookieStr.split(';');
 
         for (let i = 0; i < cas.length; i++) {
             const pair = cas[i];
-            console.log(`[${pair}]`);
+            // console.log(`[${pair}]`);
             const kv = pair.trim().split('='), key = kv[0], value = kv[1];
-            // const kv = pair.trim().split('='), key = (kv[0]).trim(), value = (kv[1]).trim();
 
             for (let j = 0; j < names.length; j++) {
                 const ele = names[j];
@@ -61,30 +59,27 @@
     // 
     function showTextUI() {
         let div = document.createElement('div');
-        div.innerHTML = "\n  <textarea rows=\"20\" cols=\"100\" id=\"configUITextArea\"></textarea>\n  <div>\n  <button id=\"configUIConvert\">\u8F6C\u6362</button>\n  <button id=\"configUIcancel\">\u53D6\u6D88</button>\n  </div>\n  ";
+        div.innerHTML = "\n  <textarea rows=\"20\" cols=\"100\" id=\"configUITextArea\"></textarea>\n  <div>\n  <button class=\"configBtn\" id=\"configUIConvert\">\u8F6C\u6362</button>\n  <button class=\"configBtn\" id=\"configUIcancel\">\u53D6\u6D88</button>\n  </div>\n  ";
         div.id = 'configUIdiv';
         div.style.cssText = "position:fixed;top:100px;z-index:100;";
         document.body.appendChild(div);
         var cancelBtn = document.querySelector('#configUIcancel');
-        cancelBtn.onclick = hideTextUI(div);
+        cancelBtn.addEventListener('click', hideTextUI(div));
         var convertBtn = document.querySelector('#configUIConvert');
-        convertBtn.onclick = excuteConvert;
+        convertBtn.addEventListener('click', excuteConvert);
     }
 
     function hideTextUI(e) {
         console.log(e);
         // var div = document.querySelector('#configUIdiv');
-        // document.body.removeChild(e);
+        document.body.removeChild(e);
     }
     function excuteConvert() {
         var cookieStr = document.querySelector('#configUITextArea').value;
         // console.log(cookieStr);
         let result = getCookies2(cookieStr, ['pt_key', 'pt_pin']);
-        console.log(result);
-        GM_setClipboard(result, {
-            type: 'text',
-            mimetype: 'text/plain',
-        });
+        // console.log(result);
+        GM_setClipboard(result, { type: 'text', mimetype: 'text/plain' });
     }
 
     function addBtn() {
